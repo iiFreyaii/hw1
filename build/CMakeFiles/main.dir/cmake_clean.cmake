e3cc8663ae7db9fca8ef457a83f3afa8fb463aec
Freyaaaaa <167000763+iiFreyaii@users.noreply.github.com>
2024-05-20 23:54:53 +0800

    2354
    firstTry

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/algebra.c.obj"
+  "CMakeFiles/main.dir/src/algebra.c.obj.d"
+  "CMakeFiles/main.dir/src/main.c.obj"
+  "CMakeFiles/main.dir/src/main.c.obj.d"
+  "E:/From GitHub/Freya_hw1/bin/libmain.dll.a"
+  "E:/From GitHub/Freya_hw1/bin/main.exe"
+  "E:/From GitHub/Freya_hw1/bin/main.exe.manifest"
+  "E:/From GitHub/Freya_hw1/bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
